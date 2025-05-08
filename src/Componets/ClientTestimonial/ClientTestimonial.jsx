feca8cce2a80c3f59ba6307daf05266e957e7d41
MeghlaB <meghlab964@gmail.com>
2025-05-09 01:03:11 +0600

    clients testimonial added

--- src/Componets/ClientTestimonial/ClientTestimonial.jsx
+++ src/Componets/ClientTestimonial/ClientTestimonial.jsx
@@ -19,13 +19,24 @@ function ClientTestimonial() {
 
   return (
     <div className='container mx-auto py-6 text-white h-[600px]'>
+    <div className='text-center space-y-3'>
+    <h1 className='text-orange-500 text-2xl font-bold'>Testimonials</h1>
+    <div>
+        <h1 className='text-2xl font-bold'>What Our Client Say <span className="text-orange-500">.</span></h1>
+    </div>
+
+    </div>
       <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
         {
           reviews.map(review => (
             <SwiperSlide key={review.id}>
               <div className="flex flex-col items-center mx-24 my-16">
+               <h1 className='border text-white px-8 py-2 rounded-2xl text-2xl border-amber-700'>
+                {review.brand}
+               </h1>
                 <p className="py-8">{review.feedback}</p>
                 <h3 className="text-2xl text-orange-400">{review.name}</h3>
+                <p className="">{review.position}</p>
               </div>
             </SwiperSlide>
           ))
